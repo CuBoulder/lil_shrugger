d41987b5b44503b9402afb3131ff4871576523a4
owenpm3 <o.morrill@aggiemail.usu.edu>
2017-06-15 09:37:09 -0600

    feature/17 Add confirm/cancel and success alert when site has been created.

--- src/js/create-site.js
+++ src/js/create-site.js
@@ -7,20 +7,28 @@ $(document).ready(function () {
   let myEl = document.getElementById('create-site');
 
   myEl.addEventListener('click', function() {
+    let current_env = getAtlasURL(document.querySelector('.env-list .selected').innerHTML);
+    current_env = current_env + 'sites';
+
+    let sure = confirm('Are you sure you want to create a site at: ' + current_env + '?');
+
     let data = JSON.stringify({
       "status": "pending"
     });
 
     let xhr = new XMLHttpRequest();
 
-    let current_env = getAtlasURL(document.querySelector('.env-list .selected').innerHTML);
-    current_env = current_env + 'sites';
+    if (sure === true) {
+      xhr.open("POST", current_env);
+      xhr.setRequestHeader("content-type", "application/json");
+      xhr.setRequestHeader("authorization", "Basic " + siteConfig['postman_token']);
 
-    xhr.open("POST", current_env);
-    xhr.setRequestHeader("content-type", "application/json");
-    xhr.setRequestHeader("authorization", "Basic " + siteConfig['postman_token']);
-
-    xhr.send(data);
+      xhr.send(data);
+      console.log(current_env);
+      alert('Creation of new site was successful.')
+    } else {
+      return;
+    }
   }, false);
 
 });
