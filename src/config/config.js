b0bdf1737b56be6361efb514c0c9023bf3d109e1
Alex Finnarn <alexfinnarn@gmail.com>
2017-06-27 11:09:42 -0600

    adding config.js back in

---
+++ src/config/config.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Contains data used in other places.
+ */
+let siteConfig = {};
+
+/**
+ * The select options can be used in the listing component for when the data/options
+ * are known. To make this work, the keys, e.g "status, code_type", map to the columns
+ * in the listing table. The keys also have to be listed in the editKeys as well
+ * as the selectKeys properties.
+ */
+siteConfig['selectOptions'] = {
+  status: ['available', 'installed', 'launching'],
+  type: ['module', 'core', 'profile'],
+};
