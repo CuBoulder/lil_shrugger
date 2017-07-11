ee9f324451e148fc650e553e6a9bb1357d9daabf
Alex Finnarn <alexfinnarn@gmail.com>
2017-07-11 16:45:28 -0600

    adding vuex for state management and extra content for when rows are edited:

--- src/js/github.js
+++ src/js/github.js
@@ -9,18 +9,7 @@
  * @returns {Array}
  */
 function getGitHubRepos() {
-  let headers = new Headers();
-  let auth = btoa(localStorage.getItem('github-username') + ':' + localStorage.getItem('github-token'));
-  headers.set('Content-Type', 'application/json');
-  headers.set('Authorization', 'Basic ' + auth);
-
-  let myInit = {
-    method: 'GET',
-    headers: headers,
-    //mode: 'cors',
-    //cache: 'default',
-    timeout: 15,
-  };
+  let myInit = initializeHeaders();
 
   return fetch('https://api.github.com/orgs/CuBoulder/repos?per_page=100', myInit)
     .then(handleErrors)
@@ -38,8 +27,6 @@ function getGitHubRepos() {
           return 0;
         });
       }
-
-
 
       // Add a default; otherwise user can't select first element.
       let first = {
@@ -59,6 +46,47 @@ function getGitHubRepos() {
  * @returns {Array}
  */
 function getRepoBranches(repo) {
+  let myInit = initializeHeaders();
+
+  return fetch('https://api.github.com/repos/CuBoulder/' + repo + '/branches?per_page=100', myInit)
+    .then(handleErrors)
+    .then(response => response.json())
+    .catch(error => error);
+}
+
+/**
+ * Takes a GitHub repo and returns the most recent commit regardless of branch.
+ *
+ * @param repo
+ * @returns {string}
+ */
+function getLatestCommit(repo, that = null) {
+  let myInit = initializeHeaders();
+
+  // Need to account for stupid Drupal repo name.
+  if (repo === 'drupal') {
+    repo = 'drupal-7.x'
+  }
+
+  return fetch('https://api.github.com/repos/CuBoulder/' + repo + '/commits', myInit)
+    .then(handleErrors)
+    .then(response => response.json())
+    .then(function (data) {
+      // Return first listed commit and passed in object if exists.
+      return {
+        hash: data[0].sha,
+        object: that
+      };
+    })
+    .catch(error => error);
+}
+
+/**
+ * Initializes common headers used in GitHub requests.
+ *
+ * @returns {{method: string, headers: Headers, timeout: number}}
+ */
+function initializeHeaders() {
   let headers = new Headers();
   let auth = btoa(localStorage.getItem('github-username') + ':' + localStorage.getItem('github-token'));
   headers.set('Content-Type', 'application/json');
@@ -72,8 +100,5 @@ function getRepoBranches(repo) {
     timeout: 15,
   };
 
-  return fetch('https://api.github.com/repos/CuBoulder/' + repo + '/branches?per_page=100', myInit)
-    .then(handleErrors)
-    .then(response => response.json())
-    .catch(error => error);
+  return myInit;
 }
