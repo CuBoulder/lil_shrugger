8b2818c727bb305224e8ea4d95a88cc58c4ddd9b
Alex Finnarn <alexfinnarn@gmail.com>
2017-07-14 11:40:09 -0600

    add paging to GitHub fetch

--- src/js/github.js
+++ src/js/github.js
@@ -11,10 +11,76 @@
 function getGitHubRepos() {
   let myInit = initializeHeaders();
 
-  return fetch('https://api.github.com/orgs/CuBoulder/repos?per_page=100', myInit)
-    .then(handleErrors)
-    .then(response => response.json())
-    .then(function(data) {
+  // This function returns the data based on the current page.
+  let foo = function (pageLink = null) {
+    // Use paging link if it exists.
+    // The paging link contains the endpoint and query.
+    let url = ''
+    if (pageLink !== null) {
+      url = pageLink
+    } else {
+      url = 'https://api.github.com/orgs/CuBoulder/repos?per_page=25'
+    }
+
+    let link = ''
+    return Promise.resolve(
+      fetch(url, myInit)
+        .then(handleErrors)
+        .then(function (response) {
+          link = response.headers
+          return response.json()
+        })
+        .then(function (data) {
+          return {
+            headers: link,
+            data: data
+          }
+        })
+        .catch((error) => {
+          bus.$emit('onMessage', {
+            text: 'Error in Atlas Request: ' + error,
+            alertType: 'alert-danger'
+          })
+          console.log(error)
+        })
+    );
+  };
+
+  // This is a wrapper function so all of the data can be returned neatly.
+  let fetchData = function () {
+    // This function does the actual looping through the paging links until
+    // it gets to the last page and then exits.
+    let recursiveFetch = function (finalData, pageLink) {
+      // Call Atlas with the correct page link.
+      return foo(pageLink)
+        .then(handleErrors)
+        .then(function (object) {
+          // By pushing the data to an array that exists in recursion, we
+          // can return the compiled array.
+          // Trying to return a variable outside this lexical scope won't work
+          // Since that would be declared synchronously.
+          finalData.push(object.data);
+
+          // Check if more pages exist.
+          let nextLink = object.headers.get('Link').split(',')[0].split(';')
+          if (nextLink[1].trim() === 'rel="next"') {
+            console.log(nextLink[0].slice(1, -1))
+            return recursiveFetch(finalData, nextLink[0].slice(1, -1))
+          } else {
+            return finalData;
+          }
+        })
+        .catch(error => console.log(error));
+    };
+
+    // Finally call recursive function and return a promise with the data in it.
+    return recursiveFetch([], null)
+  }
+
+  // Pass the resolved promise along.
+  return fetchData()
+    .then(function (data) {
+      console.log(data);
       // Sort alphabetically or by updated date if repo-listing = true.
       if (localStorage.getItem('repo-listing') === "true") {
         data.sort(function (a, b) {
@@ -22,8 +88,12 @@ function getGitHubRepos() {
         });
       } else {
         data.sort(function (a, b) {
-          if (a.name < b.name) {return -1}
-          if (a.name > b.name) {return 1}
+          if (a.name < b.name) {
+            return -1
+          }
+          if (a.name > b.name) {
+            return 1
+          }
           return 0;
         });
       }
@@ -58,6 +128,7 @@ function getRepoBranches(repo) {
  * Takes a GitHub repo and returns the most recent commit regardless of branch.
  *
  * @param repo
+ * @param that
  * @returns {string}
  */
 function getLatestCommit(repo, that = null) {
