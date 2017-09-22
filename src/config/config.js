cfcf5583419203294311ed2707265213a3332961
Alex Finnarn <alexfinnarn@gmail.com>
2017-09-22 11:42:12 -0600

    Add Way To Save Queries

--- src/config/config.js
+++ src/config/config.js
@@ -53,7 +53,7 @@ const store = new Vuex.Store({
       Test: 'https://www-test.colorado.edu/',
       Prod: 'https://www.colorado.edu/'
     },
-    userPermissions: ['row:edit', 'createSite', 'createCode', 'commands:command', 'commands:export'],
+    userPermissions: ['row:edit', 'createSite', 'createCode', 'commands:command', 'commands:export', 'statsSearch:save'],
     actionIcons: {
       sites: [{name: 'search', component: 'statsSearch'},{name: 'th-list', component: 'commands'},{name: 'refresh', component: 'table'}],
       code: [{name: 'refresh', component: 'table'}]
