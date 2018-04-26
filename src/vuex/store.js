6d73c5a75fdbd14decd90719ead1d2cd8c09d761
Alex Finnarn <alexfinnarn@gmail.com>
2018-04-26 10:09:21 -0600

    Issue #340: Add Local Environment For NewDev (#341)

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -55,6 +55,7 @@ const store = new Vuex.Store({
     env: localStorage.getItem('env') ? localStorage.getItem('env') : 'Local',
     expressEnvironments: {
       Local: 'https://express.local/',
+      NewDev: 'https://www-dev-https.colorado.edu/',
       Dev: 'https://www-dev.colorado.edu/',
       Test: 'https://www-test.colorado.edu/',
       Prod: 'https://www.colorado.edu/',
