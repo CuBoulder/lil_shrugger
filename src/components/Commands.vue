28fc7c5f0d7472b57f8f4835d81264d91829d101
Alex Finnarn <alexfinnarn@gmail.com>
2018-06-15 11:11:55 -0600

    Issue #366: remove commands event listeners

--- src/components/Commands.vue
+++ src/components/Commands.vue
@@ -61,9 +61,9 @@
     },
     beforeDestroy() {
       // Remove event listeners.
-      // bus.$off(['sendCommand', 'commandsSendCommand']);
-      // bus.$off(['validate--sendCommand', 'commandsValidateCommand']);
-      // bus.$off(['cancel--sendCommand', 'commandsCancelCommand']);
+      bus.$off(['sendCommand', 'commandsSendCommand']);
+      bus.$off(['validate--sendCommand', 'commandsValidateCommand']);
+      bus.$off(['cancel--sendCommand', 'commandsCancelCommand']);
     },
     computed: {
       commands() {
